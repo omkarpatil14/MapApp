cb577b1c73cf68808da6a23e875ddfeeecc1b397
omkar patil <omkarpatil14>
2024-02-13 10:39:28 +0530

    client added

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-red-700' >
+        hello
+      </div>
+      
+    </>
+  )
+}
+
+export default App
